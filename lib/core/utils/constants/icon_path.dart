12712ed716980856bc3276f538b411f02d2c8c3f
ahizaz <izaz3531ahmed@gmail.com>
2025-09-05 13:16:13 +0600

    beforenamaj

--- lib/core/utils/constants/icon_path.dart
+++ lib/core/utils/constants/icon_path.dart
@@ -34,7 +34,7 @@ class IconPath {
   static const plus = "assets/icons/plus.png";
   static const scantext = "assets/icons/scan-text.png";
   static const voiceai = "assets/icons/voiceai.png";///
-  static const star = "assets/icons/star.png";
+  static const star = "assets/icons/star.png";//
 
  
 }
