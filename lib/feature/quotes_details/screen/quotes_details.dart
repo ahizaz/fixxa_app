a8bf52d258a01bcfde35209396535d485b739212
ahizaz <izaz3531ahmed@gmail.com>
2025-09-27 09:58:52 +0600

    done...

--- lib/feature/quotes_details/screen/quotes_details.dart
+++ lib/feature/quotes_details/screen/quotes_details.dart
@@ -1,6 +1,11 @@
 import 'package:fixxa_app/core/utils/constants/icon_path.dart';
+import 'package:fixxa_app/core/utils/constants/image_path.dart';
 import 'package:fixxa_app/feature/quotes_details/controller/qutoe_details_controller.dart';
+import 'package:fixxa_app/feature/qutoe_invoice_createion.dart/screen/quote_creation.dart';
 import 'package:fixxa_app/feature/viewquote_edit_details/screen/view_quote_edit_details.dart';
+import 'package:fixxa_app/feature/invoice_creation_manually.dart/screen/invoice_dialog.dart';
+import 'package:fixxa_app/feature/quote_creation_manually.dart/screen/quote_dialog.dart';
+import 'package:fixxa_app/feature/scanner/screen/scanner_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
@@ -128,64 +133,44 @@ class QuotesDetails extends StatelessWidget {
                                               CrossAxisAlignment.center,
                                           children: [
                                             Row(
-                                              children: quote['statuses'].map<Widget>((
-                                                status,
-                                              ) {
+                                              children: quote['statuses'].map<Widget>((status) {
                                                 Color statusColor;
                                                 String statusText;
                                                 final amount = status['amount'];
                                                 switch (status['type']) {
                                                   case 'won':
-                                                    statusColor = const Color(
-                                                      0xff108759,
-                                                    );
+                                                    statusColor = const Color(0xff108759);
                                                     statusText = 'won';
                                                     break;
                                                   case 'lost':
-                                                    statusColor = const Color(
-                                                      0xffD9534F,
-                                                    );
+                                                    statusColor = const Color(0xffD9534F);
                                                     statusText = 'lost';
                                                     break;
                                                   default: // sent
-                                                    statusColor = const Color(
-                                                      0xff2563EB,
-                                                    );
+                                                    statusColor = const Color(0xff2563EB);
                                                     statusText = 'sent';
                                                 }
                                                 return Padding(
-                                                  padding: EdgeInsets.only(
-                                                    right: 4.w,
-                                                  ),
+                                                  padding: EdgeInsets.only(right: 4.w),
                                                   child: Container(
-                                                    padding:
-                                                        EdgeInsets.symmetric(
-                                                          horizontal: 10.w,
-                                                          vertical: 5.h,
-                                                        ),
+                                                    padding: EdgeInsets.symmetric(horizontal: 10.w, vertical: 5.h),
                                                     decoration: BoxDecoration(
                                                       color: statusColor,
-                                                      borderRadius:
-                                                          BorderRadius.circular(
-                                                            20.r,
-                                                          ),
+                                                      borderRadius: BorderRadius.circular(20.r),
                                                     ),
                                                     child: Text(
                                                       '$amount $statusText',
-                                                      style:
-                                                          GoogleFonts.urbanist(
-                                                            fontSize: 13.sp,
-                                                            color: Colors.white,
-                                                            fontWeight:
-                                                                FontWeight.w500,
-                                                          ),
+                                                      style: GoogleFonts.urbanist(
+                                                        fontSize: 13.sp,
+                                                        color: Colors.white,
+                                                        fontWeight: FontWeight.w500,
+                                                      ),
                                                     ),
                                                   ),
                                                 );
                                               }).toList(),
                                             ),
                                             const Spacer(),
-
                                             Text(
                                               '${quote['quotes']} quote${quote['quotes'] > 1 ? 's' : ''}',
                                               style: GoogleFonts.montserrat(
@@ -221,6 +206,135 @@ class QuotesDetails extends StatelessWidget {
                           ],
                         );
                       },
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+      // Bottom Navigation Bar same as invoices.dart
+      bottomNavigationBar: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 16.w),
+        child: SizedBox(
+          width: double.infinity,
+          height: 94.h + 60.h,
+          child: Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(ImagePath.mainbutton),
+                fit: BoxFit.contain,
+              ),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    const SizedBox(width: 30),
+                    Builder(
+                      builder: (context) {
+                        return InkWell(
+                          onTap: () async {
+                            final RenderBox box = context.findRenderObject() as RenderBox;
+                            final Offset position = box.localToGlobal(Offset.zero);
+                            final result = await showMenu<String>(
+                              context: context,
+                              color: const Color(0xffF2F2F2),
+                              position: RelativeRect.fromLTRB(
+                                position.dx,
+                                position.dy - 120,
+                                position.dx + 100,
+                                0,
+                              ),
+                              items: [
+                                PopupMenuItem(
+                                  value: 'quote',
+                                  child: Row(
+                                    children: [
+                                      Image(
+                                        image: AssetImage(IconPath.createquote),
+                                        height: 24.h,
+                                        width: 24.w,
+                                        fit: BoxFit.cover,
+                                      ),
+                                      const SizedBox(width: 8),
+                                      Text(
+                                        "Create Quote",
+                                        style: GoogleFonts.urbanist(
+                                          fontSize: 17.sp,
+                                          fontWeight: FontWeight.w500,
+                                          color: const Color(0xff1C1C1C),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                PopupMenuItem(
+                                  value: 'invoice',
+                                  child: Row(
+                                    children: [
+                                      Image(
+                                        image: AssetImage(IconPath.createinvoice),
+                                        height: 24.h,
+                                        width: 24.w,
+                                        fit: BoxFit.cover,
+                                      ),
+                                      const SizedBox(width: 8),
+                                      Text(
+                                        "Create Invoice",
+                                        style: GoogleFonts.urbanist(
+                                          fontSize: 17.sp,
+                                          fontWeight: FontWeight.w500,
+                                          color: const Color(0xff1C1C1C),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            );
+                            if (result == 'quote') {
+                              QuoteDialog.show(context);
+                            } else if (result == 'invoice') {
+                              InvoiceDialog.show(context);
+                            }
+                          },
+                          child: Image.asset(
+                            IconPath.plus,
+                            width: 24.w,
+                            height: 24.h,
+                            fit: BoxFit.cover,
+                          ),
+                        );
+                      },
+                    ),
+                    const SizedBox(width: 20),
+                    InkWell(
+                      onTap: () {
+                        Get.to(() => ScannerScreen());
+                      },
+                      child: Image.asset(
+                        IconPath.scantext,
+                        width: 24.w,
+                        height: 24.h,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(right: 40),
+                  child: InkWell(
+                    onTap: () {
+                      showCustomDialog(context);
+                    },
+                    child: Image.asset(
+                      IconPath.voiceai,
+                      width: 56.w,
+                      height: 56.h,
+                      fit: BoxFit.cover,
                     ),
                   ),
                 ),
